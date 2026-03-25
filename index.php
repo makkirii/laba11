13a94b5bfd983d3321853dd0244abc6d0ec0e579
Katya <l0bovakatya@yandex.ru>
2026-03-25 17:13:57 +0300

    Задание 3

--- index.php
+++ index.php
@@ -5,4 +5,7 @@ else echo "Ошибка при созании каталога test";
 if(rename("test", "www"))
     echo "<br>Папка test переименована на www";
 else echo "<br>Ошибка при переименовании папки test";
+if(rmdir("www"))
+    echo "<br>Каталог www удален";
+else echo "Ошибка при удалении каталога www";
 ?>
