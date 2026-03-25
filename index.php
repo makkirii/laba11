d5cda56040ed2b956d9677ea29b2aa8ef3652922
Katya <l0bovakatya@yandex.ru>
2026-03-25 17:11:46 +0300

    Задание 2

--- index.php
+++ index.php
@@ -2,4 +2,7 @@
 if(mkdir("test"))
     echo "Создан каталог test";
 else echo "Ошибка при созании каталога test";
+if(rename("test", "www"))
+    echo "<br>Папка test переименована на www";
+else echo "<br>Ошибка при переименовании папки test";
 ?>
