a690e5e053874beb5008ebe15d4abb70fd2bc582
Katya <l0bovakatya@yandex.ru>
2026-03-25 22:27:22 +0300

    Задание 5

--- index.php
+++ index.php
@@ -7,12 +7,19 @@ if(rename("test", "www"))
 else echo "<br>Ошибка при переименовании папки test";
 if(rmdir("www"))
     echo "<br>Каталог www удален";
-else echo "Ошибка при удалении каталога www";
+else echo "<br>Ошибка при удалении каталога www";
 $array = ['first', 'second', 'third', 'fourth', 'fifth'];
 mkdir("test");
 foreach($array as $ar) {
     $pathname = "test/" . $ar;
         mkdir($pathname);
         echo "<br>Папка $ar создана в каталоге test";
+}
+touch("opop.jpg");
+touch("ertert.jpg");
+echo "<br>Файлы с расширением .jpg:";
+foreach(glob("*.jpg") as $file)
+{
+    echo "<br>" . basename($file);
 }
 ?>
