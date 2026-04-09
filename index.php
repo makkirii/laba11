b3e53357a4f5fd2e720f981ab2e6bf11ae3cf3d4
Katya <l0bovakatya@yandex.ru>
2026-04-09 22:14:16 +0300

    Вторая половина

--- index.php
+++ index.php
@@ -2,42 +2,67 @@
 echo "--------задание 1-------- <br>";
 class Lobova_Employee
 {
-    public $name, $age, $salary;
+    public $name, $salary;
+    private $age;
+    public static $totalSalary = 0;
     public function getName()
     {
         return $this->name;
     }
-    public function getAge()
+    public function setAge($newage)
     {
-        return $this->age;
+        $this->checkAge($newage);
     }
-    public function getSalary()
+    public function getSalary($sum)
     {
-        return $this->salary;
+        self::$totalSalary += $sum;
+        return self::$totalSalary;
+    }
+    private function checkAge($newage)
+    {
+        if($newage >= 18)
+            $this->age = $newage;
+        else echo "Вам работать в нашей компании ещё рано:(<br>";
     }
 }
-$tom = new Lobova_Employee();
-$tom->name = "Tom";
-$tom->age = 30;
-$tom->salary = 100000;
-
-$jerry = new Lobova_Employee();
-$jerry->name = "Jerry";
-$jerry->age = 20;
-$jerry->salary = 500000;
+// $tom->name = "Tom";
+// $tom->age = 30;
+// $tom->salary = 100000;
+// $jerry = new Lobova_Employee();a
+// $jerry->name = "Jerry";
+// $jerry->age = 20;
+// $jerry->salary = 500000;
 echo "Создан класс и экземпляры!";
 echo "<br>--------задание 2--------<br>";
-$tomAge = $tom->age;
-$jerryAge = $jerry->age;
-$tomSalary = $tom->salary;
-$jerrySalary = $jerry->salary;
-echo "Сумма возрастов работников равна " . ($tomAge + $jerryAge) . " лет" . "<br>";
-echo "Сумма зарплат работников равна " . ($tomSalary + $jerrySalary) . " рублей";
+// $tomAge = $tom->age;
+// $jerryAge = $jerry->age;
+// $tomSalary = $tom->salary;
+// $jerrySalary = $jerry->salary;
+// echo "Сумма возрастов работников равна " . ($tomAge + $jerryAge) . " лет" . "<br>";
+// echo "Сумма зарплат работников равна " . ($tomSalary + $jerrySalary) . " рублей";
 echo "<br>--------задание 3--------<br>";
 echo "Добавлен метод getName";
 echo "<br>--------задание 4--------<br>";
 echo "Добавлен метод getAge";
 echo "<br>--------задание 5--------<br>";
 echo "Добавлен метод getSalary<br>";
-echo "Зарплата Тома равна " . $tom->getSalary() . " рублей"; 
+// echo "Зарплата Тома равна " . $tom->getSalary() . " рублей"; 
+echo "--------задание 6--------<br>";
+echo "Изменён метод getSalary<br>";
+$mimi = new Lobova_Employee();
+$mimi->getSalary(500);
+$mimii = new Lobova_Employee();
+echo "Сумма зарплаты: " . $mimii->getSalary(540);
+echo "<br>--------задание 7--------<br>";
+echo "Изменён метод getAge на setAge!";
+echo "<br>--------задание 8--------<br>";
+echo "Изменён метод setAge!<br>";
+$katya = new Lobova_Employee();
+$katya->setAge(12);
+echo "--------задание 9--------<br>";
+echo "Создан метод checkAge!";
+// $check = $katya->checkAge();
+// var_dump($check);
+echo "<br>--------задание 10--------<br>";
+echo "Изменены методы checkAge и setAge!";
 ?>
